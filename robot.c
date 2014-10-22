da7c62c210a579047c3dfe19beb0652cf3320562
townsenr17 <townsenr17@up.edu>
2014-10-22 23:28:20 +0000

    gave the robot a facelift.  Added myself to authorlist.

--- robot.c
+++ robot.c
@@ -4,7 +4,7 @@
  * A file that contains the supporting functions for main() in the
  * ~/robots example source for Software Engineering.
  *
- * @author Tanya L. Crenshaw
+ * @author Regan Townsend
  * @since August 2013
  *
  */
@@ -45,7 +45,7 @@ void robPrintMessage(int num)
 void robPrintAscii(void)
 {
   printf("    i_i    \n"
-	 "   [@_@]   \n"
+	 "   [b_b]   \n"
 	 "  /|___|\\  \n"
          "   d   b   "); 
 
